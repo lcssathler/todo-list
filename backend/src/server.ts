ed3c081566255fb485d68750bf063c8c68af2945
lcssathler <lcssathler@gmail.com>
2025-11-06 20:47:02 -0300

    first frontend commit

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import todoRoutes from './routes/todo.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/todos', todoRoutes);
