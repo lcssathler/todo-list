984e1055549090e10532a6a2fcdd65c32e0855dd
lcssathler <lcssathler@gmail.com>
2025-11-07 16:42:43 -0300

    create to-do form component

--- frontend/src/app/app/components/todo-form/todo-form.component.ts
+++ frontend/src/app/app/components/todo-form/todo-form.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css'
 })
 export class TodoFormComponent {
-  
+  title = '';
+
+  @Output() todoCreated = new EventEmitter<void>();
+
+  constructor(private todoService: TodoService) {}
+
+  create(): void {
+    if (!this.title.trim()) return;
+    this.todoService.createTodo(this.title.trim()).subscribe({
+      next: () => {
+        this.title = '';
+        this.todoCreated.emit();
+      },
+      error: (err) => console.error('Error creating new task:', err)
+    });
+  }
 }
